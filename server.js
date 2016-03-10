3c88919cb924f840fd48138cef22ff8de9db6ec6
Spencer Perry <spencerfarrallperry@gmail.com>
2016-03-09 22:01:34 -0500

    Created salesforce branch with working auth funct

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-var firebase       = require('firebase');
+var jsforce        = require('jsforce');
 
 // configuration ===========================================
 
@@ -20,6 +20,40 @@ var port = process.env.PORT || 8080;
 
 // connect to our firebase database 
 var firebase = new Firebase(db.url);
+
+//*************************************
+var oauth2 = new jsforce.OAuth2({
+
+  clientId : //client goes here 
+  clientSecret: //secret goes here 
+  redirectUri : 'https://thedreamapp.herokuapp.com/redirectUri'
+});
+
+// Spencer's working auth function//
+app.get('/oauth2/auth', function(req, res) {
+    console.log(req.body);
+  res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+  
+});
+//Trying to get access token, having troubles passing in the code gotten from auth function 
+app.get('/oauth2/callback', function(req, res) {
+  var conn = new jsforce.Connection({ oauth2 : oauth2 });
+  //var code = req.body.code;
+  console.log(req);
+  conn.authorize(code, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token, refresh token, and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.refreshToken);
+    console.log(conn.instanceUrl);
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+  });
+});
+
+//******************************************
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
